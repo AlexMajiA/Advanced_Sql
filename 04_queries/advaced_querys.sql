5f33eaed4f297fbeef94a779b9fe084eaab00091
Alejandro Martínez <amj.parrales@gmail.com>
2026-04-03 18:56:34 +0200

    Windows funtion and CTEs

--- 04_queries/advaced_querys.sql
+++ 04_queries/advaced_querys.sql
@@ -7,10 +7,10 @@ from
     h_ventas v
 
 left join d_pais p
-on v.id_pais = p.id_pais
+    on v.id_pais = p.id_pais
 
 left join d_categoria c
-on v.id_categoria = c.id_categoria
+    on v.id_categoria = c.id_categoria
 
 order by 1;
 
@@ -25,13 +25,13 @@ from
     h_ventas v
 
 left join d_pais p
-on v.id_pais = p.id_pais
+    on v.id_pais = p.id_pais
 
 left join d_categoria c
-on v.id_categoria = c.id_categoria
+    on v.id_categoria = c.id_categoria
 
 left join d_tipo_tarjeta tt
-on v.id_tipo_tarjeta = tt.id_tipo_tarjeta
+    on v.id_tipo_tarjeta = tt.id_tipo_tarjeta
 
 order by 1;
 
@@ -44,7 +44,7 @@ from
     h_ventas v
 
 left join d_pais p
-on v.id_pais = p.id_pais
+    on v.id_pais = p.id_pais
 
 group by 1;
 
@@ -58,10 +58,10 @@ from
     h_ventas v
 
 left join d_pais p
-on v.id_pais = p.id_pais
+    on v.id_pais = p.id_pais
 
 left join d_categoria c
-on v.id_categoria = c.id_categoria
+    on v.id_categoria = c.id_categoria
 
 group by 1,2
 order by 1;
@@ -76,7 +76,7 @@ FROM
     h_ventas v
 
 LEFT JOIN d_fecha f
-ON v.id_fecha = f.id_fecha
+    ON v.id_fecha = f.id_fecha
 
 GROUP BY 1
 order by 1;
@@ -95,7 +95,7 @@ LEFT JOIN d_fecha f
 ON v.id_fecha = f.id_fecha
 
 left join d_pais p
-ON v.id_pais = p.id_pais
+    ON v.id_pais = p.id_pais
 
 GROUP BY 1,2
 ORDER BY f.anyo, total_sales DESC;
@@ -109,8 +109,92 @@ from
     h_ventas v
 
 left join d_pais p
-on v.id_pais = p.id_pais
+    on v.id_pais = p.id_pais
 
 group by 1
 order by total_sales DESC
 Limit 5;
+
+
+-- Windows Functions
+-- 9. Top 3 países por ventas para cada año (con subconsulta)
+SELECT *
+FROM (
+    SELECT
+        f.anyo,
+        p.desc_pais,
+        SUM(v.ventas) AS total_sales,
+        ROW_NUMBER() OVER (
+            PARTITION BY f.anyo
+            ORDER BY SUM(v.ventas) DESC
+        ) AS ranking
+    FROM h_ventas v
+
+    LEFT JOIN d_fecha f
+        ON v.id_fecha = f.id_fecha
+    LEFT JOIN d_pais p
+        ON v.id_pais = p.id_pais
+    GROUP BY f.anyo, p.desc_pais
+
+) t
+WHERE ranking <= 3;
+
+
+-- 9. Top 3 países por ventas para cada año (con CTE)
+with ranking_table as (
+    SELECT
+        f.anyo,
+        p.desc_pais,
+        sum(v.ventas) as total_sales,
+        row_number() over(
+            partition by f.anyo
+            order by sum(v.ventas) DESC
+        ) as ranking
+    FROM
+        h_ventas v
+
+    LEFT JOIN d_fecha f
+        ON v.id_fecha = f.id_fecha
+
+    LEFT JOIN d_pais p
+        ON v.id_pais = p.id_pais
+    GROUP BY f.anyo, p.desc_pais
+) 
+
+SELECT *
+FROM ranking_table
+WHERE
+    ranking <=3;
+
+
+-- 10. Obtener el país con más ventas para cada año, mostrando:
+-- año, país, ventas totales y porcentaje que representa sobre el total de ventas de ese año.
+with base as (
+    SELECT
+        f.anyo,
+        p.desc_pais,
+        sum(v.ventas) as total_sales,
+        row_number() OVER(
+            partition by f.anyo
+            order by sum(v.ventas) DESC
+        ) as ranking,
+            count(ventas) as number_sales,
+        SUM(sum(v.ventas)) OVER( PARTITION BY f.anyo) as total_year_sales
+    FROM
+        h_ventas v
+
+    LEFT JOIN d_fecha f
+        ON v.id_fecha = f.id_fecha
+
+    LEFT JOIN d_pais p
+        ON v.id_pais = p.id_pais
+    
+    GROUP BY f.anyo, p.desc_pais
+)
+select 
+    anyo,
+    desc_pais,
+    total_sales,
+    total_sales/total_year_sales AS porcentaje
+from base
+where ranking = 1;
